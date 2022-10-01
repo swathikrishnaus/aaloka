f344e5de85acde72d8c8a88e461cd82e53f503b3
swathikrishnaus <swathikrishnaus2015@gmail.com>
2022-10-01 19:12:12 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -40,3 +40,28 @@ $(document).ready(function () {
   });
 });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const bg = document.querySelector('.background-image');
+const windowWidth = window.innerWidth / 5;
+const windowHeight = window.innerHeight / 5 ;
+
+bg.addEventListener('mousemove', (e) => {
+  const mouseX = e.clientX / windowWidth;
+  const mouseY = e.clientY / windowHeight;
+  
+  bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
+});
