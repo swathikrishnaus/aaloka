763321280052a5cf13b82a6d2fa949c32b527521
swathikrishnaus <swathikrishnaus2015@gmail.com>
2022-10-06 18:59:49 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -46,6 +46,15 @@ $(document).ready(function () {
 
 
 
+// ----------------------li active---------------------------------
+
+
+// function changeNav(cls){
+//   $("." + cls).addClass("active");
+// }
+
+
+// ------------------------li active--------------------------------
 
 
 
@@ -54,14 +63,13 @@ $(document).ready(function () {
 
 
 
+// const bg = document.querySelector('.background-image');
+// const windowWidth = window.innerWidth / 5;
+// const windowHeight = window.innerHeight / 5 ;
 
-const bg = document.querySelector('.background-image');
-const windowWidth = window.innerWidth / 5;
-const windowHeight = window.innerHeight / 5 ;
-
-bg.addEventListener('mousemove', (e) => {
-  const mouseX = e.clientX / windowWidth;
-  const mouseY = e.clientY / windowHeight;
+// bg.addEventListener('mousemove', (e) => {
+//   const mouseX = e.clientX / windowWidth;
+//   const mouseY = e.clientY / windowHeight;
   
-  bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
-});
+//   bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
+// });
