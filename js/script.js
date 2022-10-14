3d2784ffd1fa8050bc1072685e914fee4637d5bf
swathikrishnaus <swathikrishnaus2015@gmail.com>
2022-10-14 09:56:51 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -59,7 +59,11 @@ $(document).ready(function () {
 
 
 
-
+$(document).ready(function(){
+	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+		$(this).toggleClass('open');
+	});
+});
 
 
 
